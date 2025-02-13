fb2639cbf37e2e572daf290384f3ccfa400162cc
Devin Droddy <devin.droddy@gmail.com>
2025-02-13 08:55:36 -0500

    1.5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 mod lexer;
 mod token;
 
-fn main() {}
+use lexer::Lexer;
+use rustyline::error::ReadlineError;
+
+fn main() {
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+    loop {
+        match rl.readline(">> ") {
+            Ok(line) => {
+                let _ = rl.add_history_entry(&line);
+                dbg!(Lexer::new(line).collect::<Vec<_>>());
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error: {e:?}");
+                break;
+            }
+        }
+    }
+}
